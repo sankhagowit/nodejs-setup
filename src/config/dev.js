23a30c64dfdd0b45b75119d689fc3cd176c7f8fc
Bobby Laptop <bobby@sankhagowit.com>
2018-12-06 21:18:11 -0600

    feat: Config directory setup

---
+++ src/config/dev.js
@@ -0,0 +1,6 @@
+export const config = {
+  secrets: {
+    jwt: "devSecretzzz"
+  },
+  dbUrl: "mongodb://localhost:27017/dbName"
+};
---
+++ src/config/testing.js
@@ -0,0 +1,6 @@
+export const config = {
+  secrets: {
+    jwt: "testingSecretzz"
+  },
+  dbUrl: "mongodb://localhost:27017/testing-name"
+};
